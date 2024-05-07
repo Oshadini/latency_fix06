575c68cb7fd76e63f8f885e53b016ff77eacb0a4
Oshadini Kariyawasam <43576871+Oshadini@users.noreply.github.com>
2024-05-07 19:52:48 +0530

    Update MuRAGAppLatency.py

--- MuRAGAppLatency.py
+++ MuRAGAppLatency.py
@@ -206,7 +206,7 @@ if uploaded_file is not None:
       """Getting the base64 string"""
       with open(image_path, "rb") as image_file:
           return base64.b64encode(image_file.read()).decode("utf-8")
-    st.cache_data
+    st.cache_data()
     def image_summarize(img_base64, prompt):
       """Make image summary"""
       if immage_sum_model == 'gpt-4-vision-preview':
